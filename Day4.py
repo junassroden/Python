dd7104d854a64ba5a63e13f04ebcbe9acd11b796
Jhonas Cabanero <cabanerojhoas@gmail.com>
2026-06-11 06:23:36 +0800

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,91 @@
+#print("Problem 1: Print Numbers 1 to 10")
+
+#for i in range(1, 11):
+#    print(i)
+
+
+#print("Problem 2: Print Even Numbers")
+
+#for i in range(2, 21, 2):
+#    print(i)
+
+
+#print("Problem 3: Sum of First N Numbers")
+
+#n = int(input("Enter a number: "))
+#total = 0
+
+#for i in range(1, n + 1):
+#    total = total + i
+
+#print("Sum =", total)
+
+
+#print("Problem 4: Multiplication Table")
+
+#num = int(input("Enter a number: "))
+
+#for i in range(1, 11):
+#    print(num, "x", i, "=", num * i)
+
+
+#print("Problem 5: Count Down")
+
+#num = int(input("Enter a number: "))
+
+#while num >= 1:
+#    print(num)
+#    num = num - 1
+
+
+#print("Problem 6: Factorial Calculator")
+
+#num = int(input("Enter a number: "))
+#factorial = 1
+
+#for i in range(1, num + 1):
+#    factorial = factorial * i
+
+#print("Factorial =", factorial)
+
+
+#print("Problem 7: Count Digits")
+
+#num = int(input("Enter a number: "))
+#count = 0
+
+#while num > 0:
+#    num = num // 10
+#    count = count + 1
+
+#print("Number of digits =", count)
+
+
+#print("Problem 8: Reverse a Number")
+
+#num = int(input("Enter a number: "))
+#reverse = 0
+
+#while num > 0:
+#    digit = num % 10
+#    reverse = reverse * 10 + digit
+#    num = num // 10
+
+#print("Reversed number =", reverse)
+
+
+#print("Problem 9: Guessing Game")
+
+#secret = 7
+#guess = 0
+
+#while guess != secret:
+#    guess = int(input("Guess the number: "))
+
+#print("Correct!")
+
+
+#print("Problem 10: Star Pattern")
+
+#for i in range(1, 6):
+#    print("*" * i)
