0cb2b8b094c0abb6764906e96175e3f1014fccff
Jhonas Cabanero <cabanerojhoas@gmail.com>
2026-06-10 08:24:25 +0800

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,1 +1,36 @@
+#print("Hello World!");  
 
+#print("Hello! Jhonas"); 
+
+#age = 19;
+#print(f"My age is: {age}");
+
+#num1 = 10;
+#num2 = 20;
+#print(num1 + num2);
+
+#num1 = 100;
+#num2 = 300;
+#print(num1 * num2);
+
+#num1 = 100;
+#num2 = 300;
+#print(num1 / num2);
+
+#num1 = 200;
+#num2 = 3;
+#print(num1 % num2);
+
+#name1 = "anthony";
+#name2 = "bob";
+#print(f"Given Names: {name1}, {name2}");
+#name1, name2 = name2, name1
+#print(f"Swapped Names: {name1}, {name2}");
+
+#celsius = int(input("Enter Celsius: "))
+#fahrenheit = ((celsius * 9) / 5) + 32
+#print(f"Fahrenheit: {fahrenheit:.2f}")
+
+#fahrenheit = int(input("Enter Fahrenheit: "))
+#celsius = ((fahrenheit -32) * 5) / 9
+#print(f"Celsius: {celsius:.2f}")
